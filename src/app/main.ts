590ee0e4a0babaa0f3c3b37a362c6928d604ff9c
Oscar Chapa <tutoschapa@gmail.com>
2022-07-01 10:31:30 -0600

    Agregamos una interface base y la extendemos en las demas interfaces

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,10 +4,13 @@ addProduct({
     id: 12,
     title: 'Laptop',
     createdAt: new Date,
+    updatedAt: new Date,
     stock: 12,
     size: 'L',
     category: {
       id: 1,
-      name: 'electronics'
+      name: 'electronics',
+      updatedAt: new Date,
+      createdAt: new Date
     }
 })
